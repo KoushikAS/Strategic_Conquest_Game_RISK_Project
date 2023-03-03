edf99c2d6547bb1edcec5ad523f59ad36b9c7db4
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-03-03 17:00:52 -0500

    Fixed merge conflict

--- scripts/run-tests-in-docker.sh
+++ scripts/run-tests-in-docker.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 mkdir coverage
-<<<<<<< HEAD
 docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
-=======
-docker run --rm -v `pwd`/coverage:/coverage-out  citest chmod +x scripts/test.sh && scripts/test.sh
->>>>>>> test
