cc1ce98a0bd8e997734bd1d7958ce387d18dc79d
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-03-31 21:00:53 -0400

    Added another endpoint to debug

--- server/src/main/java/edu/duke/ece651/team13/server/controller/TestController.java
+++ server/src/main/java/edu/duke/ece651/team13/server/controller/TestController.java
@@ -9,7 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,18 +22,36 @@ public class TestController {
     @Autowired
     private final MapService mapService;
 
+    @Autowired
+    private final TerritoryService territoryService;
+
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
     @GetMapping("/getMap")
-    public ResponseEntity<MapEntity> getMap() {
+    public ResponseEntity<List<TerritoryEntity>> getMap() {
 
         MapEntity mapEntity = mapService.createMap(3);
         MapEntity map = mapService.getMap(mapEntity.getId());
         log.info("Count "+ map.getTerritories());
+        log.info("Count" + map.getTerritories().size());
+        List<TerritoryEntity> territoriesByMap = territoryService.getTerritoriesByMap(map);
+        return ResponseEntity.ok().body(territoriesByMap);
+    }
+
+    @GetMapping("/getMap/{id}")
+    public ResponseEntity<MapEntity> getMap(@PathVariable("id") Long id) {
+
+
+    log.info("The id recieved" + id);
+        MapEntity map = mapService.getMap(id);
+        log.info("Count "+ map.getTerritories());
+        log.info("Count" + map.getTerritories().size());
+
         return ResponseEntity.ok().body(map);
     }
+
 
 }
