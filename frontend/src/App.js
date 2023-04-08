7cbf6e9ccda94c0dd56676d2be84ffe70f9567e6
Oliver Chen <56765896+YushunChen@users.noreply.github.com>
2023-04-08 17:25:02 -0400

    Finished more routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
+import { AuthProvider, AuthContext } from "./AuthProvider";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GameView from './game/GameView';
-import AttackView from './game/AttackView';
-import LoginView from './login/LoginView'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GameView from "./game/GameView";
+import AttackView from "./game/AttackView";
+import LoginView from "./login/LoginView";
+import GameListView from "./game_list/GameListView";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<GameView />} />
-        <Route exact path="/attack" element={<AttackView />} />
-        <Route path="/login" element={<LoginView />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route
+            exact
+            path="/gameList"
+            element={
+              <ProtectedRoute>
+                <GameListView />
+              </ProtectedRoute>
+            }
+          />
+          <Route exact path="/" element={<GameView />} />
+          <Route exact path="/attack" element={<AttackView />} />
+          <Route path="/login" element={<LoginView />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ frontend/src/game_list/GameListView.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider";
+
+const GameListView = () => {
+  const { user } = useContext(AuthContext);
+  console.log("auth: " + user);
+  return <div>GameListView</div>;
+};
+
+export default GameListView;
